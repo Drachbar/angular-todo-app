69f519690b4671fa49012bf162729e535a47e81c
Mattias <drachbar@gmail.com>
2023-04-01 15:01:36 +0200

    Create a toto class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HelloComponent } from './hello/hello.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 
--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+    ) {
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,8 +17,8 @@ import { Component } from '@angular/core';
 })
 export class ListTodosComponent {
   todos = [
-    {id: 1, description : 'Learn to Dance'},
-    {id: 2, description : 'Become an Expert at angular'},
-    {id: 3, description : 'Visit India'},
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an Expert at angular', false, new Date()),
+    new Todo(3, 'Visit India', false, new Date()),
   ]
 }
